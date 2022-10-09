5421a6ac283288afbbd42c585565cf462506e9f4
LTJ <cn3085@gmail.com>
2022-10-09 22:17:33 +0900

    feat(CarPosition.java): 자동차 위치 이동 구현 및 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -23,4 +23,26 @@ public class CarTest {
     void 유효하지않은_자동차이름_입력받아_에러내기() {
         assertThrows(IllegalArgumentException.class, () -> new Cars(Arrays.asList("빨간차", "노란차차차설운도차차차")));
     }
+
+    @Test
+    void 자동차_정지(){
+        Car car1 = new Car(new CarName("빨간차"), new Drive(() -> 0));
+        car1.drive();
+        assertEquals(car1.getPosition(), Drive.STOP_POINT);
+
+        Car car2 = new Car(new CarName("빨간차"), new Drive(() -> Drive.MOVE_DECIDE_STANDARD_NUMBER - 1));
+        car2.drive();
+        assertEquals(car2.getPosition(), Drive.STOP_POINT);
+    }
+    @Test
+    void 자동차_진행(){
+        Car car1 = new Car(new CarName("빨간차"), new Drive(() -> 9));
+        car1.drive();
+        assertEquals(car1.getPosition(), Drive.MOVE_POINT);
+
+        Car car2 = new Car(new CarName("빨간차"), new Drive(() -> Drive.MOVE_DECIDE_STANDARD_NUMBER));
+        car2.drive();
+        car2.drive();
+        assertEquals(car2.getPosition(), Drive.MOVE_POINT * 2);
+    }
 }

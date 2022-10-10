db8ae89d2ab993b1951a50f341d536c5424d83f4
LTJ <cn3085@gmail.com>
2022-10-10 21:17:30 +0900

    refactor(CarName.java): 자동차 이름 유효성 검사 로직을 메소드로 분리

--- src/main/java/racingcar/model/CarName.java
+++ src/main/java/racingcar/model/CarName.java
@@ -6,13 +6,17 @@ public class CarName {
     private final String name;
 
     public CarName(String name) {
-        if (name == null || name.length() > CAR_NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 이하여야합니다!");
-        }
+        validateCarName(name);
         this.name = name.trim();
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateCarName(String name){
+        if (name == null || name.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1글자 이상, 5글자 이하여야합니다!");
+        }
     }
 }
